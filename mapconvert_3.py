2712b8fec80cc125955503d2b7ba6c3f04515d4d
jhorwinski <gricelab@derm-milla-002.med.upenn.edu>
2014-08-08 15:22:04 -0400

    converter for V1V3 1-8 complete
    made additional changes to maconvert_3 from version _2 so that it is
    compatible with the V1V3 excel format

--- mapconvert_3.py
+++ mapconvert_3.py
@@ -1,15 +1,16 @@
 #author: Joseph Horwinski
 #start date: 8/5/2014
-#Reorganizes the excel format Miseq V4 (runs 1-18) mapping files to be inline with the
+#Reorganizes the excel format Miseq V1V3 (runs 1-8) mapping files to be inline with the
 #now standardized 17 column github format. Also converts tab delimited text file source 
 #from CR to LF line breaks. 
 
-miseq=raw_input('Enter MiSeq Number (ex. 01):')
+miseq=raw_input('Enter MiSeqV1V3 Number (ex. 01):')
+sd=raw_input('Enter sequencing date (ex. 2014-01-27):')
 
-excel = '/Users/gricelab/Documents/Joseph/mapping_file_conversion/Excel/MiSeq_'
-gen = '/Users/gricelab/Documents/Joseph/mapping_file_conversion/Generated/MiSeq_'
+excel = '/Users/gricelab/Documents/Joseph/mapping_file_conversion/Excel/MiSeqV1V3_'
+gen = '/Users/gricelab/Documents/Joseph/mapping_file_conversion/Generated/MiSeqV1V3_'
 txt = '.txt'
-run = 'MiSeq_'
+run = 'MiSeqV1V3_'
 colhds = '#SampleID	BarcodeSequence	LinkerPrimerSequence	BarcodeName	ProjectID	StudyID	DateSequenced	RunName	HostSpecies	SampleType	Control RCBarcodeSequence	ReversePrimerSequence	WellPosition	PCRcycles	DNAconc(ng/ul)	Description'
 
 m = excel+miseq+txt
@@ -35,27 +36,29 @@ for line in mapfi:
 	else:
 		line=line.split()
 		first=line[0:3]
-		first.append(line[17])
-		first.append(line[9])
-		first.append(line[10])
-		first.append(line[7])
+		first.append('NA')
+		first.append(line[13])
+		first.append(line[14])
+		first.append(sd)
 		first.append(rn)
-		first.append(line[6])
-		first.append(line[4])
-		control=line[12]
+		first.append(line[10])
+		first.append(line[8])
+		control=line[16]
 		if control == 'water':
 			first.append('Water')
 		elif control == 'mock':
 			first.append('Mock')
+		elif control == 'Control':
+			first.append('Study')
 		else:
-			first.append('NA')
+			first.append('No')
 		#first.append('control?_No_Sequencing_Water_Mock')
-		first.append(line[18])
+		first.append('NA')
 		first.append(line[3])
-		first.append(line[8])
+		first.append(line[12])
 		first.append('35')
 		first.append('NA')
-		first.append(line[19])
+		first.append(line[23])
 		x='	'.join(first)
 		desfi.write(x)
 		desfi.write('\n')
