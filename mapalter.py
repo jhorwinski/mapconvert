241451d13f47ba52278e1b8f0393d6fe472a16f4
jhorwinski <gricelab@derm-milla-002.med.upenn.edu>
2014-08-18 09:00:06 -0400

    Map alter script complete

--- mapalter.py
+++ mapalter.py
@@ -1,37 +1,30 @@
 #author: Joseph Horwinski
 #start date: 8/13/2014
 #Removes 'RCBarcode' and 'BarcodeName' columns and changes column header 'DNAconc(ng/ul)' 
-#to 'DNAconc'. Adds column 'LibraryPreparer' and fills in column info. Changes 'NA's in #
-#'HostSpecies' column to 'Control'.
+#to 'DNAconc'. Changes 'NA's in'HostSpecies' column to 'Control'.
 
 
-#miseq=raw_input('Enter MiSeqV1V3 Number (ex. 01):')
-#v='MiSeqV1V3_'
+miseq=raw_input('Enter MiSeqV1V3 Number (ex. 01):')
+v='MiSeqV1V3_'
 
 
-miseq=raw_input('Enter MiSeq Number (ex. 01):')
-
-v='MiSeq_'
+#miseq=raw_input('Enter MiSeq Number (ex. 01):')
+#v='MiSeq_'
 
 txt = '.txt'
 
-#doc = '/Users/gricelab/Documents/Club_Grice/mapping_files/'
-doc = '/Users/gricelab/Documents/Joseph/mapping_file_conversion/Generated/'
+'/Documents/Club_Grice/mapping_files/run_maps/MiSeq_01.txt'
+
+doc = '/Users/gricelab/Documents/Club_Grice/mapping_files/run_maps/'
+#doc = '/Users/gricelab/Documents/Joseph/mapping_file_conversion/Generated/'
 
 m = doc+v+miseq+txt
 
 colhds = '#SampleID	BarcodeSequence	LinkerPrimerSequence	ProjectID	StudyID	DateSequenced	RunName	HostSpecies	SampleType	Control	ReversePrimerSequence	WellPosition	PCRcycles	DNAconc	Description'
 
-#mapfi = open(m, "rb").read()
-#updated = mapfi.replace("\r", "\n")
-#if updated != mapfi:
-#	f = open(m, "wb")
-#	f.write(updated)
-#	f.close()
-
 mapfi = open(m, 'r')
 
-file = open("newfile.txt", "w")
+file = open("blank.txt", "w")
 
 for line in mapfi:
 	line=line.rstrip()
@@ -42,18 +35,27 @@ for line in mapfi:
 	else:
 		line=line.split()
 		first=line[0:3]
-		first.append(line[4:8])
+		first.extend(line[4:8])
 		species = (line[8])
 		if species == 'NA':
 			first.append('Control')
 		else:
 			first.append(line[8])
-		first.append(line[9:11])
-		first.append(line[12:])
+		first.extend(line[9:11])
+		first.extend(line[12:])
 		x='	'.join(first)
 		file.write(x)
 		file.write('\n')
-file.close()
 mapfi.close()
+file.close()
 
+file2 = open("blank.txt", 'r')
 
+mapfi2 = open(m, 'w')
+
+for line in file2:
+	line=line.rstrip()
+	mapfi2.write(line)
+	mapfi2.write('\n')
+
+mapfi2.close()
